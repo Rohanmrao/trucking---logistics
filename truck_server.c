21f45c7308e0a7055d2b2130a408bdc72af692e7
Rohan <rohanmaheshrao10@gmail.com>
2022-07-18 16:26:51 +0530

    merged localdb with the the truck server

--- truck_server.c
+++ truck_server.c
@@ -4,6 +4,16 @@
 #include <time.h>
 #include <stdlib.h>
 
+typedef struct node{
+    int key;
+    struct node *next;
+}NODE;
+
+typedef struct queue{
+
+    struct node *front , *rear;
+}Q;
+
 // Defnition for all the functions that will land on the opening page  
 
 double mobileNumber(double mb)                                       //Checks whether the mobilenumber entered is 10 digit or not
@@ -308,10 +318,6 @@ void cancel_end(double mobileNumber)  // Function to cancel a job at the end of 
 	printf("	THANK YOU! \n");
 }
 
-
-
-
-
 int ID_gen(int l,int u) // Generates the SESSION LOGIN ID
 {
 	srand(time(0));
@@ -390,6 +396,81 @@ void cancel_front(double mobileNum)  // Function to cancel previously scheduled 
 
 	printf("\n");
 	printf("____THANK YOU!____ (opening new terminal...) \n");
+}
+
+NODE *create_node(int key_in){
+
+    NODE *temp = malloc(sizeof(NODE));
+    temp->key = key_in;
+    temp->next = NULL;
+
+    return temp;
+    
+}
+Q *create_queue(){
+
+    Q *q = malloc(sizeof(Q));
+    q->front = q->rear = NULL;
+
+    return q;
+}
+
+void schedule_job(Q* q, int k)
+{
+
+    printf("Enqueing %d\n",k);
+    // Create a new temporary node
+    Q *temp = create_node(k);
+  
+    // If queue is empty, then new node is front and rear both
+    if (q->front == NULL && q->rear == NULL) {
+        q->front = q->rear = temp;
+        return;
+    }
+  
+    // Add the new node at the end of queue and change rear
+    q->rear->next = temp;
+    q->rear = temp;
+    q->rear->next = NULL;
+}
+
+void delete_job(Q *q)
+{
+    // If queue is empty, return NULL.
+    if (q->front == NULL && q->rear == NULL){
+
+        printf("Empty\n");
+        return;
+    }
+  
+    // Store previous front and move front one node ahead
+    NODE *temp = q->front;
+  
+    q->front = q->front->next;
+  
+    // If front becomes NULL, then change rear also as NULL
+    if (q->front == NULL)
+        q->rear = NULL;
+  
+    printf("Deleting %d\n",temp->key);
+    free(temp);
+}
+
+void display_schedule(Q *q){
+
+    NODE *temp = q->front; // this will traverse the queue 
+    temp = q->front;
+
+    while (temp != NULL){
+        
+        temp = q->front;
+        printf("%d\n",temp->key);
+        q->front = q->front->next;
+        
+    }
+
+    free(temp);
+    temp = NULL;
 }
 
 
